0ab8e572910ff49ebdf761d172faa4be760d1304
zoarian <miko.paliszewski@gmail.com>
2022-04-08 16:28:06 +0100

    More work on the queue manager

--- FlightHack/QueueManager.cs
+++ FlightHack/QueueManager.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 using static FlightHack.Globals;
 
 namespace FlightHack
 {
     public class QueueManager
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+
         public int JobsInQueue;
         public int JobsInProgress;
         public int JobsCompleted;
+        public int JobsFailed;
 
         public static Queue<Job> JobQueue; // This will act as out job list
 
@@ -50,9 +55,39 @@ namespace FlightHack
 
         public void CheckQueueStatus()
         {
-            if(JobQueue.Peek().Status == Status.InProgress)
+
+#if DEBUG
+            log.DebugFormat("Checking Queue Status");
+#endif
+            
+            if(JobQueue.Count > 0)
             {
+                // First, check if we have any jobs
+            }
+            else
+            {
+                Job BottomOfStack = JobQueue.Peek();
+
+#if DEBUG
+                log.DebugFormat("Bottom of the queue stack. Job {0} is {1}", BottomOfStack, BottomOfStack.Status);
+#endif
+
+                if (BottomOfStack.Status == Status.InProgress)
+                {
+
+                }
+                else if(BottomOfStack.Status == Status.Completed)
+                {
+                    JobsCompleted++;
+                    JobQueue.Dequeue();
+                }
+                else if(BottomOfStack.Status == Status.Failed)
+                {
+
+                }
+
                 // We've got a job that's in progress
+                // Don't need to do anything on that front
             }
 
             bool JobsInProgress = false;
